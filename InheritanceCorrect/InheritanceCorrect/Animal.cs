f2e94b0941742e0eba8440c9d07b5bcf58b3ef88
scKeijser <tobias.keijser@studentconsulting.com>
2015-12-03 15:15:17 +0100

    Fully coded correct example

---
+++ InheritanceCorrect/InheritanceCorrect/Animal.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceCorrect
+{
+    //All property and variables are examples. Any are fine.
+    #region Assignment1
+    public class Animal
+    {
+        #region Assignment2
+        private string name;
+        private int numberOfLegs;
+        private bool isMamal;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public int NumberOfLegs 
+        {
+            get { return numberOfLegs; }
+            set { numberOfLegs = value; }
+        }
+        public bool IsMamal
+        {
+            get { return isMamal; }
+            set { isMamal = value; }
+        }
+        #endregion
+    }
+    #endregion
+    #region Assignment3&4
+    public class Horse : Animal
+    {
+        private string breed;
+        private string colour;
+
+        public string Breed
+        {
+            get { return breed; }
+            set { breed = value; }
+        }
+
+        public string Colour
+        {
+            get { return colour; }
+            set { colour = value; }
+        }
+
+    }
+    public class Dog : Animal
+    {
+        private string size;
+        private double weight;
+
+        public string Size
+        {
+            get { return size; }
+            set { size = value; }
+        }
+
+        public double Weight
+        {
+            get { return weight; }
+            set { weight = value; }
+        }
+    }
+    public class Hedgehog : Animal
+    {
+        private int numberOfSpikes;
+        private bool isHibernating;
+
+        public int NumberOfSpikes
+        {
+            get { return numberOfSpikes; }
+            set { numberOfSpikes = value; }
+        }
+
+        public bool IsHibernating
+        {
+            get { return isHibernating; }
+            set { isHibernating = value; }
+        }
+    }
+    public class Worm : Animal
+    {
+        private double length;
+        private string typeOfWorm;
+
+        public double Length
+        {
+            get { return length; }
+            set { length = value; }
+        }
+
+        public string TypeOfWorm
+        {
+            get { return TypeOfWorm; }
+            set { typeOfWorm = value; }
+        }
+    }
+    public class Bird : Animal
+    {
+        private bool birdOfPassage;
+        private string family;
+
+        public bool BirdOfPassage
+        {
+            get { return birdOfPassage; }
+            set { birdOfPassage = value; }
+        }
+
+        public string Family
+        {
+            get { return family; }
+            set { family = value; }
+        }
+    }
+    #endregion
+    //For assignment 5-10 see Program.cs
+    #region Assignment11&12
+    public class Pelican : Bird
+    {
+        private double beakSize;
+        private double wingSpan;
+
+        public double BeakSize
+        {
+            get { return beakSize; }
+            set { beakSize = value; }
+        }
+        public double WingSpan
+        {
+            get { return wingSpan; }
+            set { wingSpan = value;}
+        }
+    }
+    public class Swan : Bird
+    {
+        private bool propertyOfHMTheQueen;
+        private double neckLength;
+        public bool PropertyOfHMTheQueen
+        {
+            get { return propertyOfHMTheQueen; }
+            set { propertyOfHMTheQueen = value; }
+        }
+        public double NeckLength
+        {
+            get { return neckLength; }
+            set { neckLength = value; }
+        }
+    }
+
+    public class Flamingo : Bird
+    {
+        private bool standing;
+        private string colour;
+
+        public bool Standing
+        {
+            get { return standing; }
+            set { standing = value; }
+        }
+        public string Colour
+        {
+            get { return colour; }
+            set { colour = value; }
+        }
+    }
+    #endregion
+    //For assignment 13-16 see Program.cs
+}
---
+++ InheritanceCorrect/InheritanceCorrect/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceCorrect
+{
+    class Program
+    {
+        //For assignment 1 see Animal.cs
+        static void Main(string[] args)
+        {
+            #region Assignment5
+            List<Animal> Animals = new List<Animal>();
+            #endregion
+            #region Assignment6
+            Dog Animal1 = new Dog { Name = "Fido", IsMamal = true, NumberOfLegs = 4, Size = "Medium", Weight = 40 };
+            Bird Animal2 = new Bird { Name = "Leffe", IsMamal = false, NumberOfLegs = 2, BirdOfPassage = true, Family = "Tit" };
+            Horse Animal3 = new Horse { Name = "Golden Star", IsMamal = true, NumberOfLegs = 4, Breed = "Ardener", Colour="Black"};
+
+            Animals.Add(Animal1);
+            Animals.Add(Animal2);
+            Animals.Add(Animal3);
+            #endregion
+            #region Assignment7
+            foreach (Animal animal in Animals)
+            {
+                Console.WriteLine(animal.GetType());
+            }
+            Console.ReadKey();
+            #endregion
+            #region Assignment8-10
+            List<Dog> Dogs = new List<Dog>();
+            //Dogs.Add(Animal3); Does not compile, can't convert horses to dogs 
+            #endregion
+            //For Assignment 11-12 see Animal.cs
+            #region Assignment 13-16
+            //To store Flamingo, Pelican and Swan a list must store the type Bird
+            //To store any of our custom classes a list must store the type Animal
+            //If an attribute is for all Birds it will be put in the bird class
+            //Id an attribute is for all Animals it will be put in the Animal class
+            #endregion 
+        }
+    }
+}
+
